0d64f2c36c74ee5bee9854bcf30b2c5c1bb4babf
MichalZanzuri <154893648+MichalZanzuri@users.noreply.github.com>
2025-06-03 00:49:35 +0300

    Create changenamestables_LHDB.sql

---
+++ Stage3/Integrate/changenamestables_LHDB.sql
@@ -0,0 +1,8 @@
+ALTER TABLE customer RENAME TO customer_temp;
+ALTER TABLE discount RENAME TO discount_temp;
+ALTER TABLE product RENAME TO product_temp;
+ALTER TABLE returnproduct RENAME TO returnproduct_temp;
+ALTER TABLE sale RENAME TO sale_temp;
+ALTER TABLE saleproduct RENAME TO saleproduct_temp;
+ALTER TABLE store RENAME TO store_temp;
+ALTER TABLE employee RENAME TO employee_temp;
