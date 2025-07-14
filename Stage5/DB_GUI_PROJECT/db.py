265d32322303ed03802a2612b7b74e2f3891dcd5
MichalZanzuri <154893648+MichalZanzuri@users.noreply.github.com>
2025-07-14 15:08:07 +0300

    Update db.py

--- Stage5/DB_GUI_PROJECT/db.py
+++ Stage5/DB_GUI_PROJECT/db.py
@@ -2,7 +2,7 @@ import psycopg2
 
 def connect():
     return psycopg2.connect(
-        host="localhost",      # או השם של ה-container אם את עם Docker
+        host="localhost",
         port="5432",
         database="Mergeed_DB",
         user="myuser",
